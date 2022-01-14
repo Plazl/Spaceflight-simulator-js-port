a3b8178b5a72e43196cf1058b2bf482aabe5d117
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-14 09:33:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-var thrust
-var liquidEngines =>
 var RCS = {thrust:1.5, weight:0.5, atmosDrag:10}
 var Ion = {thrust:0.5, weight:0.5, atmosDrag:10}
 var Valliant = {thrust:50, weight:5, atmosDrag:50}
@@ -7,3 +5,16 @@ var Frontier = {thrust:150, weight:25, atmosDrag:65}
 var Hawk = {thrust:300, weight:25, atmosDrag:80}
 var Titan = {thrust:450, weight:50, atmosDrag:100}
 var Raptor = {thrust:500, weight:55, atmosDrag:110}
+var liquidEngines = {
+RCS:RCS,
+Ion:Ion,
+Valliant:Valliant,
+Frontier:Frontier,
+Hawk:Hawk,
+Titan:Titan,
+Raptor:Raptor 
+}
+
+var game = document.createElement("canvas");
+var ctx = canvas.getConttext("2d");
+var background = document.getElementById("background_img")
