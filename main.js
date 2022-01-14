c176aade10f43fd56b1eee7b21ed9c3fd052cac9
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-14 13:00:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var enginesOn = false;
+var throttle = 0;
+var throttlePercent;
 var RCS = {thrust:1.5, weight:0.5, atmosDrag:10}
 var Ion = {thrust:0.5, weight:0.5, atmosDrag:10}
 var Valliant = {thrust:50, weight:5, atmosDrag:50}
@@ -14,7 +17,23 @@ Hawk:Hawk,
 Titan:Titan,
 Raptor:Raptor 
 }
+var seperator = {power:15, weight:1, atmosDrag:2}
 
 var game = document.createElement("canvas");
-var ctx = canvas.getConttext("2d");
+var ctx = game.getContext("2d");
 var background = document.getElementById("background_img")
+var defaultRocket = {
+  stg1engine:Raptor,
+  stg1seperator:seperator,
+  stg2engine:Frontier,
+  stg2seperator:seperator,
+  stg3engine:Valliant}
+function updater() {
+  throttlePercent = percentage(throttle, 0)
+  function percentage(throttle, totalValue) {
+   return (100 * throttle) / 100;
+} 
+}
+if (enginesOn == true && throttle >0) {
+  
+}
