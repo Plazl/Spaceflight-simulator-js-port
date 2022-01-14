2d1c2e40a8e5419341615f139d971eec57b91081
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-14 08:53:03 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-const liquidEngines = ["RCS", "Ion", "Vallient", "Frontier", "Hawk", "Titan", "Raptor";
-]
+var thrust
+var liquidEngines =>
+var RCS = {thrust:1.5, weight:0.5, atmosDrag:10}
+var Ion = {thrust:0.5, weight:0.5, atmosDrag:10}
+var Valliant = {thrust:50, weight:5, atmosDrag:50}
+var Frontier = {thrust:150, weight:25, atmosDrag:65}
+var Hawk = {thrust:300, weight:25, atmosDrag:80}
+var Titan = {thrust:450, weight:50, atmosDrag:100}
+var Raptor = {thrust:500, weight:55, atmosDrag:110}
