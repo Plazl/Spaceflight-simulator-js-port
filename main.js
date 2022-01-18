f348d54c89e082eae533f325494d92bc36e160d5
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-18 10:20:47 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var enginesOn = false;
 var throttle = 0;
 var throttlePercent;
-var RCS = {thrust:1.5, weight:0.5, atmosDrag:10}
-var Ion = {thrust:0.5, weight:0.5, atmosDrag:10}
-var Valliant = {thrust:50, weight:5, atmosDrag:50}
-var Frontier = {thrust:150, weight:25, atmosDrag:65}
-var Hawk = {thrust:300, weight:25, atmosDrag:80}
-var Titan = {thrust:450, weight:50, atmosDrag:100}
-var Raptor = {thrust:500, weight:55, atmosDrag:110}
+var RCS = {thrust:1.5, weight:0.5, atmosDrag:10};
+var Ion = {thrust:0.5, weight:0.5, atmosDrag:10};
+var Valliant = {thrust:50, weight:5, atmosDrag:50};
+var Frontier = {thrust:150, weight:25, atmosDrag:65};
+var Hawk = {thrust:300, weight:25, atmosDrag:80};
+var Titan = {thrust:450, weight:50, atmosDrag:100};
+var Raptor = {thrust:500, weight:55, atmosDrag:110};
 var liquidEngines = {
 RCS:RCS,
 Ion:Ion,
@@ -16,16 +16,15 @@ Frontier:Frontier,
 Hawk:Hawk,
 Titan:Titan,
 Raptor:Raptor 
-}
+};
 var totalWeight;
-var fuelTank = {height:10, width:5, weight:10}
-var a
-var Capsule; 
-var seperator = {power:15, weight:1, atmosDrag:2}
+var fuelTank = {height:10, width:5, weight:10};
+var a;
+var Capsule = {weight:4}; 
+var seperator = {power:15, weight:1, atmosDrag:2};
 
 var game = document.createElement("canvas");
 var ctx = game.getContext("2d");
-var background = document.getElementById("background_img")
 var defaultRocket = {
   stg1engine:Raptor,
   fueltank1:fuelTank,
@@ -36,7 +35,7 @@ var defaultRocket = {
   fueltank3:fuelTank,
   stg3engine:Valliant,
   stg3seperator:seperator,
-  capsule:Capsule}
+  capsule:Capsule};
 var maxStage = 3;
 var currentStage;
 var stage1thrust = defaultRocket.stg1engine;
@@ -46,39 +45,40 @@ var stage3On;
 game.addEventListener('keydown', function (no){
  if (event.key === '32') {
     activateStage();  
- }
+ };
 });
 function activateStage() {
-  if (currentStage < maxStage)
-    currentStage = currentStage + 1
+  if (currentStage < maxStage);
+    currentStage = currentStage + 1;
     if (currentStage == 1) {
-      stage1On = true
-    }
+      stage1On = true;
+    };
   
     if (currentStage == 2) {
-      stage2On = true
-      stage1On = false
-    }
+      stage2On = true;
+      stage1On = false;
+    };
   
     if (currentStage == 3) {
-      stage3On = true
-      stage2On = false
-    }
-}
+      stage3On = true;
+      stage2On = false;
+    };
+};
  
 function updater() {
-  throttlePercent = percentage(throttle, 0)
+  throttlePercent = percentage(throttle, 0);
   function percentage(throttle, totalValue) {
    return (100 * throttle) / 100;
-} 
-  totalWeight = defaultRocket.stg1engine.weight + defaultRocket.stg1fuelTank.weight + defaultRocket.stg1seperator.weight + defaultRocket.stg2engine.weight + defaultRocket.stg2fuelTank.weight + defaultRocket.stg2seperator.weight + defaultRocket.stg3engine.weight + defaultRocket.stg3fuelTank.weight + defaultRocket.stg3seperator.weight + defaultRocket.capsule.weight
-}
+};
+  totalWeight = Raptor.weight + fuelTank.weight + seperator.weight + Frontier.weight + fuelTank.weight + seperator.weight + Valliant.weight + fuelTank.weight + seperator.weight + capsule.weight
+  
+};
 if (enginesOn == true && throttle >0) {
   if (stage1On == true) {
-    a = stg1engine.thrust / totalWeight 
-  }
-}
+    a = stg1engine.thrust / totalWeight;
+  };
+};
 
 function no() {
- return
-}
+ return;
+};
