b8fc2fa07b413539b0817da6aba8799a75b587fd
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-18 09:58:42 -0500

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ Hawk:Hawk,
 Titan:Titan,
 Raptor:Raptor 
 }
+var totalWeight;
+var fuelTank = {height:10, width:5, weight:10}
+var a
+var Capsule; 
 var seperator = {power:15, weight:1, atmosDrag:2}
 
 var game = document.createElement("canvas");
@@ -24,16 +28,57 @@ var ctx = game.getContext("2d");
 var background = document.getElementById("background_img")
 var defaultRocket = {
   stg1engine:Raptor,
+  fueltank1:fuelTank,
   stg1seperator:seperator,
+  fueltank2:fuelTank,
   stg2engine:Frontier,
   stg2seperator:seperator,
-  stg3engine:Valliant}
+  fueltank3:fuelTank,
+  stg3engine:Valliant,
+  stg3seperator:seperator,
+  capsule:Capsule}
+var maxStage = 3;
+var currentStage;
+var stage1thrust = defaultRocket.stg1engine;
+var stage1On;
+var stage2On;
+var stage3On;
+game.addEventListener('keydown', function (no){
+ if (event.key === '32') {
+    activateStage();  
+ }
+});
+function activateStage() {
+  if (currentStage < maxStage)
+    currentStage = currentStage + 1
+    if (currentStage == 1) {
+      stage1On = true
+    }
+  
+    if (currentStage == 2) {
+      stage2On = true
+      stage1On = false
+    }
+  
+    if (currentStage == 3) {
+      stage3On = true
+      stage2On = false
+    }
+}
+ 
 function updater() {
   throttlePercent = percentage(throttle, 0)
   function percentage(throttle, totalValue) {
    return (100 * throttle) / 100;
 } 
+  totalWeight = defaultRocket.stg1engine.weight + defaultRocket.stg1fuelTank.weight + defaultRocket.stg1seperator.weight + defaultRocket.stg2engine.weight + defaultRocket.stg2fuelTank.weight + defaultRocket.stg2seperator.weight + defaultRocket.stg3engine.weight + defaultRocket.stg3fuelTank.weight + defaultRocket.stg3seperator.weight + defaultRocket.capsule.weight
 }
 if (enginesOn == true && throttle >0) {
-  
+  if (stage1On == true) {
+    a = stg1engine.thrust / totalWeight 
+  }
+}
+
+function no() {
+ return
 }
